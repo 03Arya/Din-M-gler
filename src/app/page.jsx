6fb02677984b7932df2d1a131cb8ca8611fad7b7
03Arya <142574149+03Arya@users.noreply.github.com>
2024-02-29 13:48:31 +0100

    footer in progress, style and component file made

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,19 +4,22 @@ import "@/style/nav.scss"
 import "@/style/hero.scss"
 import "@/style/aboutUs.scss"
 import "@/style/ad.scss"
+import "@/style/footer.scss"
 
 import Hero from "@/components/hero";
 import Navigation from "@/components/navigation";
 import AboutUs from "@/components/aboutUs";
 import Ad from "@/components/ad";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <main>
-      <Navigation />
+      <Navigation />      
       <Hero />
       <AboutUs />
       <Ad />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="bottomFooter">
+                <p>Layout by Jit Banik 2020</p>
+            </div>
+        </footer>
+    )
+}
